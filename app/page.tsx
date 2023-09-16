50223c9ec3eaaf1df1df467580b8854d1a1d1798
Matthew <mrk443@stern.nyu.edu>
2023-09-15 17:38:13 -0700

    fixed some shit

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,82 @@ import * as cheerio from "cheerio";
 
 const lastDate = dayjs().startOf("day").subtract(3, "day").toDate();
 
+export default async function Home() {
+  const data = await fetchData();
+
+  return (
+    <main className="main">
+      <h2>Top News</h2>
+      {data.map(
+        (
+          { url, first_cast, rest_of_casts, cleaned_text, hostname, metadata, first_timestamp, last_timestamp },
+          index
+        ) => (
+          <div className="main--card col max-w" key={index}>
+            <span>
+              <Link target="_blank" href={`https://warpcast.com/${first_cast.author.fname}`}>
+                <label>@{first_cast.author.fname}</label>
+              </Link>
+              <label> / </label>
+              <Link target="_blank" href={`https://${hostname}`}>
+                <label>{hostname}</label>
+              </Link>
+              <Link
+                target="_blank"
+                href={`https://warpcast.com/${first_cast.author.fname}/0x${first_cast.hash.slice(0, 6)}`}
+                style={{ marginLeft: ".5rem", color: "inherit", opacity: 0.66 }}
+              >
+                View cast →
+              </Link>
+            </span>
+            <div className="max-w row-sb-fs" style={{ gap: "1rem" }}>
+              <div className="col flex" style={{ gap: ".33rem" }}>
+                <Link target="_blank" href={first_cast.embedded_urls[0]}>
+                  <h1 className="line-3">{cleaned_text || metadata.title || url}</h1>
+                </Link>
+                <span className="line-3">
+                  {cleaned_text && (
+                    <span>
+                      {metadata.title}
+                      {metadata.description && <span> • </span>}
+                    </span>
+                  )}
+                  {metadata.description}
+                </span>
+                {rest_of_casts.length > 0 && (
+                  <p>
+                    <b style={{ color: "#408840" }}>More: </b>
+                    {rest_of_casts.map(({ author, hash }: any, i: number) => (
+                      <Fragment key={author.fid}>
+                        {i > 0 && <span>, </span>}
+                        <Link target="_blank" href={`https://warpcast.com/${author.fname}/0x${hash.slice(0, 6)}`}>
+                          <span>@{author.fname}</span>
+                        </Link>
+                      </Fragment>
+                    ))}
+                  </p>
+                )}
+                <span style={{ opacity: 0.66 }}>{dayjs(last_timestamp).fromNow()}</span>
+              </div>
+              {metadata.image && (
+                <img
+                  alt="img"
+                  src={metadata.image}
+                  height={80}
+                  width={100}
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              )}
+            </div>
+          </div>
+        )
+      )}
+    </main>
+  );
+}
+
 async function fetchData() {
   const casts = await prisma.cast.findMany({
     where: {
@@ -48,7 +124,6 @@ async function fetchPageMetadata(url: string) {
 
     return { title, description, image };
   } catch (error) {
-    console.error("Fetch error:", error);
     return { title: null, description: null, image: null };
   }
 }
@@ -115,73 +190,4 @@ function removeOrReplaceUrl(inputStr: string) {
   }
 
   return inputStr.replace(/[:;\s]+$/, "").trim();
-}
-
-export default async function Home() {
-  const data = await fetchData();
-
-  return (
-    <main className="main">
-      <h2>Top News</h2>
-      {data.map(
-        (
-          { url, first_cast, rest_of_casts, cleaned_text, hostname, metadata, first_timestamp, last_timestamp },
-          index
-        ) => (
-          <div className="main--card col max-w" key={index}>
-            <span>
-              <Link target="_blank" href={`https://warpcast.com/${first_cast.author.fname}`}>
-                <label>@{first_cast.author.fname}</label>
-              </Link>
-              <label> / </label>
-              <Link target="_blank" href={`https://${hostname}`}>
-                <label>{hostname}</label>
-              </Link>
-            </span>
-            <div className="max-w row-sb-fs" style={{ gap: "1rem" }}>
-              <div className="col flex" style={{ gap: ".33rem" }}>
-                <Link target="_blank" href={first_cast.embedded_urls[0]}>
-                  <h1 className="line-3">{cleaned_text || metadata.title || url}</h1>
-                </Link>
-                <span className="line-3">
-                  {cleaned_text && (
-                    <span>
-                      {metadata.title}
-                      {metadata.description && <span> • </span>}
-                    </span>
-                  )}
-                  {metadata.description}
-                </span>
-                {rest_of_casts.length > 0 && (
-                  <p>
-                    <b style={{ color: "#408840" }}>More: </b>
-                    {rest_of_casts.map(({ author, hash }: any, i: number) => (
-                      <Fragment key={author.fid}>
-                        {i > 0 && <span>, </span>}
-                        <Link target="_blank" href={`https://warpcast.com/${author.fname}/${hash.slice(0, 8)}`}>
-                          <span>@{author.fname}</span>
-                        </Link>
-                      </Fragment>
-                    ))}
-                  </p>
-                )}
-                <span style={{ opacity: 0.66 }}>{dayjs(last_timestamp).fromNow()}</span>
-              </div>
-              {metadata.image && (
-                <img
-                  alt="img"
-                  src={metadata.image}
-                  height={80}
-                  width={100}
-                  style={{
-                    objectFit: "cover",
-                  }}
-                />
-              )}
-            </div>
-          </div>
-        )
-      )}
-    </main>
-  );
 }
